cf4aa6949bc4f39d614457ce3b3b826c9943986f
bb7talla@gmail.com <bhaskar@bhaskars-MacBook-Air.local>
2026-03-20 21:54:09 +0530

    Initial commit for PWA

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -6,6 +6,12 @@ import { themeManager } from './theme/theme.ts'
 
 themeManager.init()
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(console.error)
+  })
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
