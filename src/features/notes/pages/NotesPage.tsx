5a7db7db6b207f040b8a04c975702a58625cdc68
bb7talla@gmail.com <bhaskar@bhaskars-MacBook-Air.local>
2026-03-21 19:24:51 +0530

    cite.config.ts file updated for improving the build performance

--- src/features/notes/pages/NotesPage.tsx
+++ src/features/notes/pages/NotesPage.tsx
@@ -1,11 +1,14 @@
 import type { NoteDataType } from '@/types'
 import { useNotes } from '@/src/features/notes/hooks/useNotes'
 import { useAuth } from '@/src/features/auth/hooks/useAuth'
-import { NoteCard, NoteControls } from '../components'
+import { NoteControls } from '../components'
 import { GuestBanner } from '@/src/features/ui/header/GuestBanner'
 import styles from '../components/noteCard/NoteCard.module.css'
 import EmptyCanvas from '@/src/features/ui/empty-canvas'
 import Spinner from '@/src/shared/components/uikit/spinner'
+import { lazy, Suspense } from 'react'
+
+const NoteCard = lazy(() => import('../components/noteCard'))
 
 const NotesPage = () => {
   const { notes, isNotesLoading } = useNotes()
@@ -21,20 +24,19 @@ const NotesPage = () => {
       className={`${styles.notes_canvas} grid_bg`}
     >
       {notes.length ? (
-        <>
-          {notes.map((note: NoteDataType, index: number) => (
-            <NoteCard
-              key={`${note.id}_${index}`}
-              note={note}
-            />
-          ))}
-        </>
+        notes.map((note: NoteDataType, index: number) => (
+          <Suspense
+            key={`${note.id}_${index}`}
+            fallback={null}
+          >
+            <NoteCard note={note} />
+          </Suspense>
+        ))
       ) : (
         <EmptyCanvas />
       )}
       <NoteControls />
-
-      {isGuest && notes.length && <GuestBanner />}
+      {isGuest && notes.length > 0 && <GuestBanner />}
     </div>
   )
 }
